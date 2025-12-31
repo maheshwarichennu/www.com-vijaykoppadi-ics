588a15d8b56931fc95742a6c558435eb0d8a38d4
maheshwarichennu <maheshwarichennu@gmail.com>
2025-12-31 15:24:12 +0530

    Add files via upload

---
+++ mahi6511/challange1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>              // Includes the standard input/output library
+
+int main() {                    // Main function where program execution starts
+    printf("Name: maheswari\n");     // Prints the name followed by a new line
+    printf("Age: 15\n");        // Prints the age followed by a new line
+    printf("Country: india\n"); // Prints the country followed by a new line
+    return 0;                   // Ends the program successfully
+}
